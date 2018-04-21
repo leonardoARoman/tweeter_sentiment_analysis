2ed64111731401ac9479987f5893ca4c077c1669
Leonardo Roman <leonardoroman@Leonardos-Air.home>
2018-04-21 14:09:54 -0400

    twitter API connection stablish using port 4040

--- tweeter_sentiment_analysis_app/src/main/app/com/leonardo/roman/SparkStreamingTweets.scala
+++ tweeter_sentiment_analysis_app/src/main/app/com/leonardo/roman/SparkStreamingTweets.scala
@@ -1,13 +1,65 @@
 package com.leonardo.roman
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import java.lang.Math
-import java.lang.Math
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.StreamingContext._
+import org.apache.spark.streaming.twitter.TwitterUtils
+
 
 object SparkStreamingTweets {
 	def main(args: Array[String]){
-		// 1. Create Spark configuration
+		if( args.length < 4){
+			println("Error!") 
+		}
+		StreamingExamples.setStreamingLogLevels()
+		println("Hello Spark-Scala")
+		var i = 0
+		for(i <- 0 to 3 ){
+			println("Args["+i+"]: "+args.apply(i))
+		}
+
+		val Array(consumerKey, consumerSecret,accessToken,accessTokenSecret) = args.take(4)
+		val filters = args.takeRight(args.length - 4)
+
+		System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+		System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+		System.setProperty("twitter4j.oauth.accessToken", accessToken)
+		System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+
+		val sparkConf = new SparkConf().setAppName("tweeter_sentiment_analysis").setMaster("local[2]")
+		val stream_sc = new StreamingContext(sparkConf, Seconds(2))
+		val stream = TwitterUtils.createStream(stream_sc, None, filters)
+		val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
+
+		val topCount60 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(60))
+				            .map{case (topic, count) => (count, topic)}
+		                .transform(_.sortByKey(false))
+		
+		val topCount10 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(10))
+		                .map{case (topic, count) => (count, topic)}
+		                .transform(_.sortByKey(false))
+
+		topCount60.foreachRDD(rdd => {
+		  val topList = rdd.take(10)
+			println("Popular topics in last 60 seconds (%s total):".format(rdd.count()))
+			topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+		})
+		
+		
+		topCount10.foreachRDD(rdd => {
+		  val topList = rdd.take(10)
+			println("Popular topics in last 10 seconds (%s total):".format(rdd.count()))
+			topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+		})
+		
+		
+		stream_sc.start()
+		stream_sc.awaitTermination()
+	}
+}
+
+/*
+// 1. Create Spark configuration
 		val conf = new SparkConf()
 				.setAppName("SparkMe Application")
 				.setMaster("local[*]")  // local mode
@@ -22,5 +74,4 @@ object SparkStreamingTweets {
 
 				counts.count()
 				counts.collect()
-	}
-}
+ */
