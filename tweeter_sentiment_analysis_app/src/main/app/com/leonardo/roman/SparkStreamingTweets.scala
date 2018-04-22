c6a8121aa70d0ce860b8698b844a6454e24d66a9
Leonardo Roman <leonardoroman@Leonardos-Air.home>
2018-04-22 13:46:41 -0400

    new changes

--- tweeter_sentiment_analysis_app/src/main/app/com/leonardo/roman/SparkStreamingTweets.scala
+++ tweeter_sentiment_analysis_app/src/main/app/com/leonardo/roman/SparkStreamingTweets.scala
@@ -1,10 +1,14 @@
 package com.leonardo.roman
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 import org.apache.spark._
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.StreamingContext._
+
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
 
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
 
 object SparkStreamingTweets {
 	def main(args: Array[String]){
@@ -27,51 +31,32 @@ object SparkStreamingTweets {
 		System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
 
 		val sparkConf = new SparkConf().setAppName("tweeter_sentiment_analysis").setMaster("local[2]")
-		val stream_sc = new StreamingContext(sparkConf, Seconds(2))
+		val stream_sc = new StreamingContext(sparkConf, Seconds(60))
+		val stream = TwitterUtils.createStream(stream_sc, None)
+		val hashTags = stream.map(status => status.getText())//.filter(_.startsWith("#")))
+		hashTags.saveAsTextFiles("/Users/leonardoroman/Desktop/data_science_projects/intro_to_DS/tweeter_sentiment_analysis/streamed_data/60sc_data")
+
+		/*
 		val stream = TwitterUtils.createStream(stream_sc, None, filters)
 		val hashTags = stream.flatMap(status => status.getText.split(" ").filter(_.startsWith("#")))
 
-		val topCount60 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(60))
+		val topCount60 = hashTags
+		                .map((_,1))
+		                .reduceByKeyAndWindow(_+_, Seconds(60))
 				            .map{case (topic, count) => (count, topic)}
 		                .transform(_.sortByKey(false))
-		
-		val topCount10 = hashTags.map((_,1)).reduceByKeyAndWindow(_+_, Seconds(10))
-		                .map{case (topic, count) => (count, topic)}
-		                .transform(_.sortByKey(false))
+
+
 
 		topCount60.foreachRDD(rdd => {
-		  val topList = rdd.take(10)
-			println("Popular topics in last 60 seconds (%s total):".format(rdd.count()))
-			topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+			    val topList = rdd.take(10)
+					println("Popular topics in last 60 seconds (%s total):".format(rdd.count()))
+					topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
+			    topCount60.saveAsTextFiles("/Users/leonardoroman/Desktop/data_science_projects/intro_to_DS/tweeter_sentiment_analysis/streamed_data/60sc_data")
 		})
-		
-		
-		topCount10.foreachRDD(rdd => {
-		  val topList = rdd.take(10)
-			println("Popular topics in last 10 seconds (%s total):".format(rdd.count()))
-			topList.foreach{case (count, tag) => println("%s (%s tweets)".format(tag, count))}
-		})
-		
+		*/
 		
 		stream_sc.start()
 		stream_sc.awaitTermination()
 	}
 }
-
-/*
-// 1. Create Spark configuration
-		val conf = new SparkConf()
-				.setAppName("SparkMe Application")
-				.setMaster("local[*]")  // local mode
-
-				// 2. Create Spark context
-				val sc = new SparkContext(conf)
-
-				val text_file = sc.textFile("/Users/leonardoroman/Desktop/data_science_projects/intro_to_DS/spark_mapreduce_tutorial/big_file.txt")
-				val counts = text_file.flatMap(line => line.split(" "))
-				.map(word => (word, 1))
-				.reduceByKey(_+_)
-
-				counts.count()
-				counts.collect()
- */
